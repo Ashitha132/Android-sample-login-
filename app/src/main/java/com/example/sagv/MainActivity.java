cfa170a414f934f75139118b93956e842865a19c
Ashitha132 <ashithaom132@gmail.com>
2019-12-06 11:10:38 -0500

    button click completed

--- app/src/main/java/com/example/sagv/MainActivity.java
+++ app/src/main/java/com/example/sagv/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.sagv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText e1,e2;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.uname);
+        e2=(EditText)findViewById(R.id.passwd);
+        b=(Button)findViewById(R.id.loginbtn);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"login succesful",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
